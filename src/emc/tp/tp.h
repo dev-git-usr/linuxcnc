1520f448a440be9f2021369f101b80a7e9222fbc
Michael <michael.langer5@web.de>
2021-02-15 09:36:42 +0100

    Update tp.h
    changed to typedef

--- src/emc/tp/tp.h
+++ src/emc/tp/tp.h
@@ -20,25 +20,25 @@
 #include "tcq.h"
 
 typedef int (*tpCreate_t)(TP_STRUCT * const tp, int _queueSize, TC_STRUCT * const tcSpace);
-int tpClear(TP_STRUCT * const tp);
-int tpInit(TP_STRUCT * const tp);
-int tpClearDIOs(TP_STRUCT * const tp);
-int tpSetCycleTime(TP_STRUCT * tp, double secs);
-int tpSetVmax(TP_STRUCT * tp, double vmax, double ini_maxvel);
-int tpSetVlimit(TP_STRUCT * tp, double limit);
-int tpSetAmax(TP_STRUCT * tp, double amax);
-int tpSetId(TP_STRUCT * tp, int id);
-int tpGetExecId(TP_STRUCT * tp);
-struct state_tag_t tpGetExecTag(TP_STRUCT * const tp);
-int tpSetTermCond(TP_STRUCT * tp, int cond, double tolerance);
-int tpSetPos(TP_STRUCT * tp, EmcPose const * const pos);
-int tpAddCurrentPos(TP_STRUCT * const tp, EmcPose const * const disp);
-int tpSetCurrentPos(TP_STRUCT * const tp, EmcPose const * const pos);
-int tpRunCycle(TP_STRUCT * tp, long period);
-int tpPause(TP_STRUCT * tp);
-int tpResume(TP_STRUCT * tp);
-int tpAbort(TP_STRUCT * tp);
-int tpAddRigidTap(TP_STRUCT * const tp,
+typedef int (*tpClear_t)(TP_STRUCT * const tp);
+typedef int (*tpInit_t)(TP_STRUCT * const tp);
+typedef int (*tpClearDIOs_t)(TP_STRUCT * const tp);
+typedef int (*tpSetCycleTime_t)(TP_STRUCT * tp, double secs);
+typedef int (*tpSetVmax_t)(TP_STRUCT * tp, double vmax, double ini_maxvel);
+typedef int (*tpSetVlimit_t)(TP_STRUCT * tp, double limit);
+typedef int (*tpSetAmax_t)(TP_STRUCT * tp, double amax);
+typedef int (*tpSetId_t)(TP_STRUCT * tp, int id);
+typedef int (*tpGetExecId_t)(TP_STRUCT * tp);
+typedef struct state_tag_t (*tpGetExecTag_t)(TP_STRUCT * const tp);
+typedef int (*tpSetTermCond_t)(TP_STRUCT * tp, int cond, double tolerance);
+typedef int (*tpSetPos_t)(TP_STRUCT * tp, EmcPose const * const pos);
+typedef int (*tpAddCurrentPos_t)(TP_STRUCT * const tp, EmcPose const * const disp);
+typedef int (*tpSetCurrentPos_t)(TP_STRUCT * const tp, EmcPose const * const pos);
+typedef int (*tpRunCycle_t)(TP_STRUCT * tp, long period);
+typedef int (*tpPause_t)(TP_STRUCT * tp);
+typedef int (*tpResume_t)(TP_STRUCT * tp);
+typedef int (*tpAbort_t)(TP_STRUCT * tp);
+typedef int (*tpAddRigidTap_t)(TP_STRUCT * const tp,
         EmcPose end,
         double vel,
         double ini_maxvel,
@@ -46,26 +46,26 @@ int tpAddRigidTap(TP_STRUCT * const tp,
         unsigned char enables,
         double scale,
         struct state_tag_t tag);
-int tpAddLine(TP_STRUCT * const tp, EmcPose end, int canon_motion_type,
+typedef int (*tpAddLine_t)(TP_STRUCT * const tp, EmcPose end, int canon_motion_type,
 	      double vel, double ini_maxvel, double acc, unsigned char enables,
 	      char atspeed, int indexrotary, struct state_tag_t tag);
-int tpAddCircle(TP_STRUCT * const tp, EmcPose end, PmCartesian center,
+typedef int (*tpAddCircle_t)(TP_STRUCT * const tp, EmcPose end, PmCartesian center,
 		PmCartesian normal, int turn, int canon_motion_type, double vel,
 		double ini_maxvel, double acc, unsigned char enables,
 		char atspeed, struct state_tag_t tag);
-int tpGetPos(TP_STRUCT const  * const tp, EmcPose * const pos);
-int tpIsDone(TP_STRUCT * const tp);
-int tpQueueDepth(TP_STRUCT * const tp);
-int tpActiveDepth(TP_STRUCT * const tp);
-int tpGetMotionType(TP_STRUCT * const tp);
-int tpSetSpindleSync(TP_STRUCT * const tp, int spindle, double sync, int wait);
-void tpToggleDIOs(TC_STRUCT * const tc); //gets called when a new tc is taken from the queue. it checks and toggles all needed DIO's
+typedef int (*tpGetPos_t)(TP_STRUCT const  * const tp, EmcPose * const pos);
+typedef int (*tpIsDone_t)(TP_STRUCT * const tp);
+typedef int (*tpQueueDepth_t)(TP_STRUCT * const tp);
+typedef int (*tpActiveDepth_t)(TP_STRUCT * const tp);
+typedef int (*tpGetMotionType_t)(TP_STRUCT * const tp);
+typedef int (*tpSetSpindleSync_t)(TP_STRUCT * const tp, int spindle, double sync, int wait);
+typedef void (*tpToggleDIOs_t)(TC_STRUCT * const tc); //gets called when a new tc is taken from the queue. it checks and toggles all needed DIO's
 
-int tpSetAout(TP_STRUCT * const tp, unsigned char index, double start, double end);
-int tpSetDout(TP_STRUCT * const tp, int index, unsigned char start, unsigned char end); //gets called to place DIO toggles on the TC queue
+typedef int (*tpSetAout_t)(TP_STRUCT * const tp, unsigned char index, double start, double end);
+typedef int (*tpSetDout_t)(TP_STRUCT * const tp, int index, unsigned char start, unsigned char end); //gets called to place DIO toggles on the TC queue
 
-int tpSetRunDir(TP_STRUCT * const tp, tc_direction_t dir);
-int tpIsMoving(TP_STRUCT const * const tp);
+typedef int (*tpSetRunDir_t)(TP_STRUCT * const tp, tc_direction_t dir);
+typedef int (*tpIsMoving_t)(TP_STRUCT const * const tp);
 
 // for jog-while-paused:
 typedef int (*tpIsPaused_t)(TP_STRUCT * tp);
