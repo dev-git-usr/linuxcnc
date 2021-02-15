68e937eee57d5a2efa59765dc0a38788d3644669
Michael <michael.langer5@web.de>
2021-02-15 22:29:16 +0100

    Update control.c

--- src/emc/motion/control.c
+++ src/emc/motion/control.c
@@ -286,7 +286,7 @@ void emcmotController(void *arg, long period)
      ((p1).v == (p2).v) &&		 \
      ((p1).w == (p2).w))
 
-extern int inRange(EmcPose pos, int id, char *move_type); // from command.c
+extern static int inRange(EmcPose pos, int id, char *move_type); // from command.c
 
 static void update_offset_pose(void)
 {
