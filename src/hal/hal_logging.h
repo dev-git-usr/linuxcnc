202e80e6059fe157aae3d9ee0e7d85f9110f17ed
Michael <michael.langer5@web.de>
2021-02-23 21:11:52 +0100

    Update hal_logging.h

--- src/hal/hal_logging.h
+++ src/hal/hal_logging.h
@@ -5,26 +5,19 @@
 
 // checking & logging shorthands
 #define HALERR(fmt, ...)					\
-    rtapi_print_msg(RTAPI_MSG_ERR,__FUNCTION__,__LINE__,	\
-		    "HAL error:", fmt, ## __VA_ARGS__)
+    rtapi_print_msg(RTAPI_MSG_ERR, "HAL error:")
 #define HALWARN(fmt, ...)					\
-    rtapi_print_msg(RTAPI_MSG_WARN,__FUNCTION__,__LINE__,	\
-		    "HAL WARNING:", fmt, ## __VA_ARGS__)
+    rtapi_print_msg(RTAPI_MSG_WARN, "HAL WARNING:")
 #define HALINFO(fmt, ...)					\
-    rtapi_print_msg(RTAPI_MSG_INFO,__FUNCTION__,__LINE__,	\
-		    "HAL info:", fmt, ## __VA_ARGS__)
+    rtapi_print_msg(RTAPI_MSG_INFO, "HAL info:")
 
 #define HALDBG(fmt, ...)					\
-    rtapi_print_msg(RTAPI_MSG_DBG,__FUNCTION__,__LINE__,	\
-		    "HAL:", fmt, ## __VA_ARGS__)
+    rtapi_print_msg(RTAPI_MSG_DBG, "HAL:")
 
 #define HAL_ASSERT(x)						\
     do {							\
 	if (!(x)) {						\
-	    rtapi_print_msg(RTAPI_MSG_ERR,			\
-			    __FUNCTION__,__LINE__,		\
-			    "HAL error:",			\
-			    "ASSERTION VIOLATED: '%s'", #x);	\
+	    rtapi_print_msg(RTAPI_MSG_ERR, "HAL error: ASSERTION VIOLATED: '%s'", #x);	\
 	}							\
     } while(0)
 
@@ -32,10 +25,7 @@
 #define CHECK_HALDATA()					\
     do {						\
 	if (hal_data == 0) {				\
-	    rtapi_print_msg(RTAPI_MSG_ERR,		\
-			    __FUNCTION__,__LINE__,	\
-			    "HAL error:",		\
-			    "called before init");	\
+	    rtapi_print_msg(RTAPI_MSG_ERR, "HAL error: called before init");	\
 	    return -EINVAL;				\
 	}						\
     } while (0)
@@ -43,9 +33,7 @@
 #define CHECK_NULL(p)						\
     do {							\
 	if (p == NULL) {					\
-	    rtapi_print_msg(RTAPI_MSG_ERR,			\
-			    __FUNCTION__,__LINE__,"HAL error:",	\
-			    #p  " is NULL");			\
+	    rtapi_print_msg(RTAPI_MSG_ERR,"HAL error: is null");			\
 	    return -EINVAL;					\
 	}							\
     } while (0)
@@ -53,9 +41,7 @@
 #define CHECK_LOCK(ll)							\
     do {								\
 	if (hal_data->lock & ll) {					\
-	    rtapi_print_msg(RTAPI_MSG_ERR,				\
-			    __FUNCTION__, __LINE__,"HAL error:",	\
-			    "called while HAL is locked (%d)",		\
+	    rtapi_print_msg(RTAPI_MSG_ERR,"HAL error: called while HAL is locked (%d)",		\
 			    ll);					\
 	    return -EPERM;						\
 	}								\
@@ -65,8 +51,7 @@
 #define CHECK_STR(name)							\
     do {								\
 	if ((name) == NULL) {						\
-	    rtapi_print_msg(RTAPI_MSG_ERR,__FUNCTION__, __LINE__,"HAL error:", \
-			    "argument '" # name  "' is NULL");		\
+	    rtapi_print_msg(RTAPI_MSG_ERR,"HAL error: argument '" # name  "' is NULL");		\
 	    return -EINVAL;						\
 	}								\
     } while(0)
@@ -76,8 +61,7 @@
     do {								\
 	CHECK_STR(name);						\
 	if (strlen(name) > len) {					\
-	    rtapi_print_msg(RTAPI_MSG_ERR,__FUNCTION__, __LINE__,"HAL error:", \
-			    "argument '%s' too long (%zu/%d)",		\
+	    rtapi_print_msg(RTAPI_MSG_ERR,"HAL error: argument '%s' too long (%zu/%d)",		\
 			    name, strlen(name), len);			\
 	    return -EINVAL;						\
 	}								\
@@ -85,10 +69,7 @@
 
 #define NOMEM(fmt, ...)						\
     do {							\
-	rtapi_print_msg(RTAPI_MSG_ERR,				\
-			__FUNCTION__, __LINE__,"HAL error:",	\
-			" insufficient memory for: "  fmt,	\
-			## __VA_ARGS__);			\
+	rtapi_print_msg(RTAPI_MSG_ERR,"HAL error: insufficient memory for: ");			\
 	return -ENOMEM;						\
     } while(0)
 
