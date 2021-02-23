9502dc1cccea0d2b690f42eb7ac4bab019bcfaed
Michael <michael.langer5@web.de>
2021-02-23 10:11:44 +0100

    Update emcmotcfg.h

--- src/emc/motion/emcmotcfg.h
+++ src/emc/motion/emcmotcfg.h
@@ -66,6 +66,8 @@
  * a TC_STRUCT is about 512 bytes so this queue is
  * about a megabyte.  */
 #define DEFAULT_TC_QUEUE_SIZE 2000
+//JWP
+#define DEFAULT_ALT_TC_QUEUE_SIZE 100   // size of secondary motion queue
 
 /* max following error */
 #define DEFAULT_MAX_FERROR 100
